f2100ef2be16826ef2d4aabc5433d3602a130ae3
projektir <oprojektir@gmail.com>
2019-12-01 12:37:45 +0100

    aoc01

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,6 +1,5 @@
-use std::io::{self, Read, Write};
-use std::io::{prelude::*, BufReader};
 use std::fs::File;
+use std::io::{prelude::*, BufReader};
 
 type Result<T> = ::std::result::Result<T, Box<dyn (::std::error::Error)>>;
 
@@ -8,9 +7,27 @@ fn main() -> Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
 
+    let mut sum: i32 = 0;
+
     for line in reader.lines() {
-        writeln!(io::stdout(), "{}", line?)?;
+        let mass: i32 = line?.parse::<i32>()?;
+
+        let fuel = fuel(mass);
+        sum += fuel;
     }
 
+    println!("{}", sum);
+
     Ok(())
+}
+
+fn fuel(mass: i32) -> i32 {
+    let mut fuel_mass = mass / 3 - 2;
+
+    if fuel_mass > 0 {
+        fuel_mass += fuel(fuel_mass);
+        fuel_mass
+    } else {
+        0
+    }
 }
